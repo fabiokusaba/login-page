348110315c4609c243fc03d52d5875a75cc26864
Fabio Kusaba <fabinhokb44@gmail.com>
2024-06-30 17:50:49 -0300

    feat: add LoginForm interface

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -6,6 +6,11 @@ import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { ToastrService } from 'ngx-toastr';
 
+interface LoginForm {
+  email: FormControl,
+  password: FormControl
+}
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -22,7 +27,7 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class LoginComponent {
 
-  loginForm!: FormGroup;
+  loginForm!: FormGroup<LoginForm>;
 
   constructor(
     private router: Router,
